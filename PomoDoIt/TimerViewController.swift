e28ef956baa2c3f522c90d97300647e9f9806329
Ryan Oldford <ryan.oldford@gmail.com>
2020-05-18 23:51:32 -0700

    Storyboard now has basic controls, linked to view controller

---
+++ PomoDoIt/TimerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TimerViewController.swift
+//  PomoDoIt
+//
+//  Created by Ryan Oldford on 2020-05-18.
+//  Copyright © 2020 Ryan Oldford. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    @IBOutlet weak var phaseLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var btnTimer: UIButton!
+    @IBOutlet weak var btnDoneList: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
