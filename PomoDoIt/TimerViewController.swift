bb98fd202e740793ca0387372badc41b8399514c
Ryan Oldford <ryan.oldford@gmail.com>
2020-05-19 00:28:38 -0700

    Timer view controller now has beginning properties

--- PomoDoIt/TimerViewController.swift
+++ PomoDoIt/TimerViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 
 class TimerViewController: UIViewController {
+    // MARK: Enumerations
+    enum Phase: String {
+        case work = "Work"
+        case shortBreak = "Short Break"
+        case longBreak = "Long Break"
+    }
+    enum TimerButtonState: String {
+        case start = "Start"
+        case pause = "Pause"
+        case resume = "Resume"
+    }
+    enum QuestionCase: String {
+        case past = "What did you just work on?"
+        case present = "What are you working on now?"
+    }
+    
+    // MARK: Outlets
     @IBOutlet weak var phaseLabel: UILabel!
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
@@ -16,9 +33,18 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var btnTimer: UIButton!
     @IBOutlet weak var btnDoneList: UIButton!
     
+    // MARK: Properties
+    var phase: Phase = Phase.work
+    var timerBtnState: TimerButtonState = TimerButtonState.start
+    var questionCase: QuestionCase = QuestionCase.present
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        phaseLabel.text = phase.rawValue
+        questionLabel.text = questionCase.rawValue
+        btnTimer.setTitle(timerBtnState.rawValue, for: .normal)
+        
     }
 
 
